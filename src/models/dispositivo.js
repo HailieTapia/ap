4e631945aeb9edfe94b53bdb90af0b0d50958960
HailieTapia <htapiahailie@gmail.com>
2024-03-23 16:36:24 -0600

    modified:   src/models/dispositivo.js

--- src/models/dispositivo.js
+++ src/models/dispositivo.js
@@ -1,10 +1,30 @@
-const mongoose=require('mongoose')
-const dispositivoSchema=mongoose.Schema(
-    {
-        modelo:{type:String,require:false},
-        estado:{type:String,require:false},
-    }
-);
+const mongoose = require('mongoose');
 
-const Dispositivo=mongoose.model('Dispositivo',dispositivoSchema)    
-module.exports=Dispositivo;
+// Esquema para datos del dispositivo
+const dispositivoSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  temperatura: {
+    type: Number,
+    required: true
+  },
+  humedad: {
+    type: Number,
+    required: true
+  },
+  estadoFoco: Boolean,
+  estadoCerradura: Boolean,
+  estadoVentilador: Boolean,
+  estadoVentilador2: Boolean,
+  fechaHora: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Crear el modelo 'Dispositivo' usando el esquema definido anteriormente
+const Dispositivo = mongoose.model('Dispositivo', dispositivoSchema);
+
+module.exports = Dispositivo;
