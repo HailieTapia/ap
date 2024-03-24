d0037c6515f84235c34790162451e73a568e4ccb
HailieTapia <htapiahailie@gmail.com>
2024-03-24 11:43:01 -0600

    modified:   src/routes/usuarios.js

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -131,7 +131,7 @@ router.post('/usuarios/solicitar-recuperacion', async (req, res) => {
         );
 
         // URL de recuperación de contraseña con el token como parámetro
-        const enlaceRecuperacion = `https://ap-pi.vercel.app/api/usuarios/cambiar-contrasena/${tokenRecuperacion}`;
+        const enlaceRecuperacion = `https://ap-pi.vercel.app/api/usuarios/cambiar-contrasena${tokenRecuperacion}`;
 
         // Configuración del correo electrónico
         const mailOptions = {
