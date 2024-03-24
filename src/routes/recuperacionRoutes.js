455d67199ebf0de36ed334c8fffc7ce590347c5e
HailieTapia <htapiahailie@gmail.com>
2024-03-23 22:10:46 -0600

    modified:   package-lock.json 	modified:   package.json 	new file:   src/routes/recuperacionRoutes.js

---
+++ src/routes/recuperacionRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const esquema = require('../models/usuarios');
+const router = express.Router();
+
+// Configuración del transportador de nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "proyeqtocuatri@gmail.com",
+        pass: "proyequi1254",
+    },
+});
+
+// Endpoint para solicitar recuperación de contraseña
+router.post('/usuarios/solicitar-recuperacion', async (req, res) => {
+    const { correo } = req.body;
+    const usuario = await esquema.findOne({ correo });
+
+    if (!usuario) {
+        return res.status(404).json({ error: 'No se encontró un usuario con ese correo electrónico.' });
+    }
+
+    // Generación del token de recuperación
+    const tokenRecuperacion = jwt.sign(
+        { _id: usuario._id },
+        'contraseñapass1234', // Aquí deberías usar process.env.JWT_SECRET_RECUPERACION
+        { expiresIn: '1h' }
+    );
+
+    // URL de recuperación de contraseña
+    const enlaceRecuperacion = `http://localhost:3000/registrarse/recuperar-contrasena/${tokenRecuperacion}`;
+
+    // Configuración del correo electrónico
+    const mailOptions = {
+        from: 'proyeqtocuatri@gmail.com', // Aquí deberías usar process.env.EMAIL_USERNAME
+        to: correo,
+        subject: 'Recuperación de Contraseña',
+        html: `<p>Hola ${usuario.nombre},</p>
+                <p>Has solicitado restablecer tu contraseña. Por favor, sigue el siguiente enlace para establecer una nueva:</p>
+                <a href="${enlaceRecuperacion}">Restablecer contraseña</a>`,
+    };
+
+    // Envío del correo electrónico
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return res.status(500).json({ error: 'Error al enviar el correo electrónico.' });
+        } else {
+            res.json({ message: 'Se ha enviado un correo electrónico con las instrucciones para restablecer tu contraseña.' });
+        }
+    });
+});
+
+module.exports = router;
