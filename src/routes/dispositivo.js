fcd7d8ca1d40fbc8f6a0f9d6bcc75850b6676505
HailieTapia <htapiahailie@gmail.com>
2024-03-23 21:27:51 -0600

    modified:   src/routes/dispositivo.js

--- src/routes/dispositivo.js
+++ src/routes/dispositivo.js
@@ -35,21 +35,50 @@ routerd.get('/dispositivo/:id',(req,res)=>{
 
 //actualizar dispositivo
 // Actualizar dispositivo
-routerd.put('/dispositivo/:id', (req, res) => {
+
+routerd.put('/dispositivo/:id', async (req, res) => {
     const { id } = req.params;
-    const { nombre, temperatura, humedad, estadoFoco, estadoCerradura, estadoVentilador, estadoVentilador2 } = req.body;
-    esquema.findByIdAndUpdate(id, {
-      nombre,
-      temperatura,
-      humedad,
-      estadoFoco,
-      estadoCerradura,
-      estadoVentilador,
-      estadoVentilador2
-    }, { new: true }) // Devuelve el documento actualizado
-    .then(data => res.json(data))
-    .catch(error => res.json({ message: error }));
+    const { estadoFoco, estadoCerradura, estadoVentilador, estadoVentilador2 } = req.body;
+    try {
+        await esquema.findByIdAndUpdate(id, {
+          estadoFoco,
+          estadoCerradura,
+          estadoVentilador,
+          estadoVentilador2
+        }, { new: true });
+
+        // Publicar el estado actualizado al topic MQTT
+        if(estadoFoco !== undefined) client.publish('Entrada/01', estadoFoco ? "focoOn" : "focoOff");
+        if(estadoCerradura !== undefined) client.publish('Entrada/01', estadoCerradura ? "cerraduraOpen" : "cerraduraClose");
+        if(estadoVentilador !== undefined) client.publish('Entrada/01', estadoVentilador ? "ventiladorOn" : "ventiladorOff");
+        if(estadoVentilador2 !== undefined) client.publish('Entrada/01', estadoVentilador2 ? "ventilador2On" : "ventilador2Off");
+
+        res.json({ message: "Dispositivo actualizado y mensaje MQTT enviado." });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
+
+// Nuevo endpoint para enviar comandos a dispositivos específicos
+routerd.post('/dispositivo/comando/:id', (req, res) => {
+    const { id } = req.params; // ID del dispositivo (si necesario para lógica específica)
+    const { comando } = req.body; // Asume que el comando viene en el cuerpo de la solicitud
+
+    // Aquí deberías tener alguna lógica para asegurarte de que el comando y el ID son válidos
+    // Por ejemplo, verificar que el dispositivo existe, que el comando es soportado, etc.
+
+    // Publicar el comando al topic MQTT
+    // Asegúrate de publicar al topic correcto y formatear el mensaje según lo espera tu dispositivo
+    client.publish('Entrada/01', comando, (error) => {
+        if(error) {
+            console.error("Error al publicar mensaje MQTT", error);
+            return res.status(500).json({ message: "Error al enviar comando MQTT." });
+        }
+        res.json({ message: "Comando enviado con éxito." });
+    });
+});
+
+
 
 //eliminar dispositivo
 routerd.delete('/dispositivo/:id',(req,res)=>{
