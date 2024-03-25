718875ebb159a1442008ccdc852d41c7e7a78190
HailieTapia <htapiahailie@gmail.com>
2024-03-25 02:22:15 -0600

    modified:   src/routes/dispositivo.js

--- src/routes/dispositivo.js
+++ src/routes/dispositivo.js
@@ -9,6 +9,65 @@ const routerd=express.Router()
 
 
 
+client.on('connect', () => {
+    client.subscribe('Entrada/01/estado', (err) => {
+        if (!err) {
+            console.log("Subscrito con éxito al topic del estado del dispensador");
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    // Suponiendo que el topic es "dispensador/estado"
+    if (topic === "Entrada/01/estado") {
+        const estado = JSON.parse(message.toString()); // Parsea el mensaje a JSON
+        const dispositivoId = "65ff5db2656ceb696b6022da"; // Asumiendo un ID de dispositivo fijo para el ejemplo
+
+        // Actualizar la base de datos con los nuevos estados
+        esquema.updateOne({_id: dispositivoId}, {$set: { 
+        temperatura: estado.temperatura,
+        humedad: estado.humedad,
+        estadoFoco: estado.estadoFoco ? "encendido" : "apagado",
+        estadoVentilador: estado.estadoVentilador ? "encendido" : "apagado",
+        estadoVentilador2: estado.estadoVentilador2 ? "encendido" : "apagado",
+        estadoCerradura: estado.estadoCerradura ? "abierta" : "cerrada"
+
+             // Asume un booleano para el botón de agua
+        }})
+        .then(result => console.log("Actualización exitosa", result))
+        .catch(error => console.error("Error al actualizar el dispositivo", error));
+    }
+});
+routerd.post('/dispositivo/comando/:id', (req, res) => {
+    const { id } = req.params; // ID del dispositivo
+    const { comando } = req.body; // Comando enviado en el cuerpo de la solicitud
+
+    const dispositivoIdValido = "65ff5db2656ceb696b6022da";
+
+    //LA LOGICA APLICADA AQUI PARA VERIFICAR EL ID, LO VAMOS A OCUPAR 
+    //PARA VERIFICAR SI EL DISPOSITIVO COINCIDE CON ALGUNA QUE TENGA EL USUARIO
+
+
+    // Verificar que el ID del dispositivo es el esperado
+    if (id !== dispositivoIdValido) {
+        // Si el ID no coincide, enviar una respuesta de error
+        return res.status(400).json({ message: "ID de dispositivo inválido." });
+    }
+
+    // Si el ID es válido, proceder a publicar el comando al topic MQTT
+    client.publish('Entrada/01', comando, (error) => {
+        if (error) {
+            console.error("Error al publicar mensaje MQTT", error);
+            return res.status(500).json({ message: "Error al enviar comando MQTT." });
+        }
+        res.json({ message: "Comando enviado con éxito." });
+    });
+});
+
+
+
+
+
 
 routerd.get('/dispositivo/prueba',(req,res)=>{
     res.json({"response":"Prueba Disp"})
@@ -42,29 +101,6 @@ routerd.post('/dispositivo/temperatura', async (req, res) => {
     console.log('Temperatura recibida:', temperatura);
     res.status(200).send('Temperatura recibida con éxito');
 });
-
-
-// Nuevo endpoint para enviar comandos a dispositivos específicos
-routerd.post('/dispositivo/comando/:id', (req, res) => {
-    const { id } = req.params; // ID del dispositivo (si necesario para lógica específica)
-    const { comando } = req.body; // Asume que el comando viene en el cuerpo de la solicitud
-
-    // Aquí deberías tener alguna lógica para asegurarte de que el comando y el ID son válidos
-    // Por ejemplo, verificar que el dispositivo existe, que el comando es soportado, etc.
-
-    // Publicar el comando al topic MQTT
-    // Asegúrate de publicar al topic correcto y formatear el mensaje según lo espera tu dispositivo
-    client.publish('Entrada/01', comando, (error) => {
-        if(error) {
-            console.error("Error al publicar mensaje MQTT", error);
-            return res.status(500).json({ message: "Error al enviar comando MQTT." });
-        }
-        res.json({ message: "Comando enviado con éxito." });
-    });
-});
-
-
-
 //eliminar dispositivo
 routerd.delete('/dispositivo/:id',(req,res)=>{
     const{id}=req.params;
