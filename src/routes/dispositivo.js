92922ae7d1a2bcea794071e487c62ea349aa701a
HailieTapia <htapiahailie@gmail.com>
2024-03-24 21:21:37 -0600

    modified:   src/routes/dispositivo.js

--- src/routes/dispositivo.js
+++ src/routes/dispositivo.js
@@ -1,73 +1,57 @@
-const express=require('express')
-const esquema=require('../models/dispositivo')
+const express = require('express');
+const esquema = require('../models/dispositivo');
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://broker.emqx.io:1883');  
+const client = mqtt.connect('mqtt://broker.emqx.io:1883');
 
+const routerd = express.Router();
 
+routerd.get('/dispositivo/prueba', (req, res) => {
+    res.json({"response": "Prueba Disp"});
+});
 
-const routerd=express.Router()
-
-routerd.get('/dispositivo/prueba',(req,res)=>{
-    res.json({"response":"Prueba Disp"})
-})
-
-routerd.post('/dispositivo',(req,res)=>{
-    const us= esquema(req.body);
+routerd.post('/dispositivo', (req, res) => {
+    const us = esquema(req.body);
     us.save()
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
+    .then(data => res.json(data))
+    .catch(error => res.json({message: error}));
+});
 
-//leer dispositivo
-routerd.get('/dispositivo',(req,res)=>{
+routerd.get('/dispositivo', (req, res) => {
     esquema.find()
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
+    .then(data => res.json(data))
+    .catch(error => res.json({message: error}));
+});
 
-//buscar dispositivo
-routerd.get('/dispositivo/:id',(req,res)=>{
-    const {id}=req.params
+routerd.get('/dispositivo/:id', (req, res) => {
+    const {id} = req.params;
     esquema.findById(id)
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-}) 
+    .then(data => res.json(data))
+    .catch(error => res.json({message: error}));
+});
 
 routerd.post('/dispositivo/temperatura', async (req, res) => {
-    const { temperatura } = req.body;
-    // Guarda la temperatura en la base de datos o haz lo que necesites con ella
+    const {temperatura} = req.body;
     console.log('Temperatura recibida:', temperatura);
     res.status(200).send('Temperatura recibida con éxito');
 });
 
-
-// Nuevo endpoint para enviar comandos a dispositivos específicos
 routerd.post('/dispositivo/comando/:id', (req, res) => {
-    const { id } = req.params; // ID del dispositivo (si necesario para lógica específica)
-    const { comando } = req.body; // Asume que el comando viene en el cuerpo de la solicitud
-
-    // Aquí deberías tener alguna lógica para asegurarte de que el comando y el ID son válidos
-    // Por ejemplo, verificar que el dispositivo existe, que el comando es soportado, etc.
-
-    // Publicar el comando al topic MQTT
-    // Asegúrate de publicar al topic correcto y formatear el mensaje según lo espera tu dispositivo
+    const {id} = req.params;
+    const {comando} = req.body;
     client.publish('Entrada/01', comando, (error) => {
         if(error) {
             console.error("Error al publicar mensaje MQTT", error);
-            return res.status(500).json({ message: "Error al enviar comando MQTT." });
+            return res.status(500).json({message: "Error al enviar comando MQTT."});
         }
-        res.json({ message: "Comando enviado con éxito." });
+        res.json({message: "Comando enviado con éxito."});
     });
 });
 
+routerd.delete('/dispositivo/:id', (req, res) => {
+    const {id} = req.params;
+    esquema.deleteOne({_id: id})
+    .then(data => res.json(data))
+    .catch(error => res.json({message: error}));
+});
 
-
-//eliminar dispositivo
-routerd.delete('/dispositivo/:id',(req,res)=>{
-    const{id}=req.params;
-    esquema.deleteOne({_id:id})
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
-
-module.exports=routerd
+module.exports = routerd;
