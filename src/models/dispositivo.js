9063746eec252c36a4809ae0870888b637206fb3
HailieTapia <htapiahailie@gmail.com>
2024-04-03 19:53:35 -0600

    modified:   src/models/dispositivo.js

--- src/models/dispositivo.js
+++ src/models/dispositivo.js
@@ -8,6 +8,7 @@ const dispositivoSchema = mongoose.Schema({
     estadoVentilador: { type: String, required: false }, // Campo estadoVentilador
     estadoVentilador2: { type: String, required: false }, // Campo estadoVentilador2
     fechaUltimoComando: { type: Date, required: false }, // Campo para almacenar la fecha y hora del último comando
+    fechaMovimientoHuevos: { type: Date, required: false }, // Campo para almacenar la fecha y hora del último movimiento de huevos
 });
 
 
