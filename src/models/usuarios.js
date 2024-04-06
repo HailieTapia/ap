b9639499d441fe1737630af549d654c0188cae90
HailieTapia <136946909+HailieTapia@users.noreply.github.com>
2024-04-05 23:24:58 -0600

    Update usuarios.js

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -10,7 +10,7 @@ const usuarioSchema = mongoose.Schema({
     preguntaRecuperacion: { type: String, require: true },
     respuestaPregunta: { type: String, require: true },
     codigoRecuperacion: { type: String },
-    dispositivos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dispositivo' }], // Referencia a dispositivos
+    dispositivos: { type: String, require: true },
 });
 
 module.exports = mongoose.model('usuario', usuarioSchema);
