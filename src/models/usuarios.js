c24db8cf57be5b3d8a116c6f3f38f3b707f2f764
HailieTapia <htapiahailie@gmail.com>
2024-04-06 16:30:26 -0600

    modified:   src/models/usuarios.js

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -12,7 +12,7 @@ const usuarioSchema=mongoose.Schema(
         preguntaRecuperacion:{type:String,require:true},
         respuestaPregunta:{type:String,require:true},
         codigoRecuperacion:{type:String},
-        dispositivos: [{ type: Schema.Types.ObjectId, ref: 'Dispositivo' }], // Referencia a dispositivos
+        dispositivos: [{ type: Schema.Types.ObjectId, ref: 'Dispositivo' }] // Referencia a dispositivos
     }
 )
 module.exports=mongoose.model('usuario',usuarioSchema)
