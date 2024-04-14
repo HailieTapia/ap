6c3d0db8bbbc789697aa3f526deb547ee92668f7
HailieTapia <htapiahailie@gmail.com>
2024-04-13 20:58:48 -0600

    modified:   src/routes/dispositivo.js

--- src/routes/dispositivo.js
+++ src/routes/dispositivo.js
@@ -35,7 +35,7 @@ client.on('message', (topic, message) => {
             estadoCerradura: estado.cerradura,
             estadoVentilador: estado.ventilador1,
             estadoVentilador2: estado.ventilador2,
-            controlAutomatico:estado.controlAutomatico
+            
         }})
         .then(result => console.log("Actualización exitosa", result))
         .catch(error => console.error("Error al actualizar el dispositivo", error));
